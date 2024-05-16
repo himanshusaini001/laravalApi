7d7aebeda56a791eb62e1f76ea38d8d0e93f4312
himanshu <himanshusaini26112002@gmail.com>
2024-05-16 16:54:06 +0530

    modified

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class ApiController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            // Add Validation Multiap type
+            $validation = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+            ]);
+
+            // Validation All Data
+            if ($validation->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation error',
+                    'errors' => $validation->errors()->all(),
+                ],401);
+            }
+
+            // Create All Data
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            //Response Data in Json
+            return response()->json([
+                'status' => true,
+                'message' => 'User created successfully',
+                'token' => $user->createToken('Api Token')->plainTextToken,
+            ],200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'An error occurred',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function login(Request $request){
+        try{
+            $validation = Validator::make($request->all(),[
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            if($validation->fails())
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation error',
+                    'errors' => $validation->errors()->all(),
+                ],401);
+            }
+            if(!Auth::attempt($request->only('email','password')))
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Emial & password Dose not match With Out record.',
+                ],401);
+            }
+            $user = User::Where('email',$request->email)->first();
+            return response()->json([
+                'status' => true,
+                'message' => 'User login is successfully',
+                'token' => $user->createToken('Api Token')->plainTextToken,
+            ], 200);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'An error occurred',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function logout(){
+        dd(auth()->user());
+        auth()->user()->token()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged Out',
+            'data'=> [],
+        ], 200);
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Register
+Route::post("register",[ApiController::class,"register"]);
+
+// Login
+Route::post("login",[ApiController::class,"login"]);
+
+//logout
+
+Route::group([
+    "middleware" =>["auth:sanctum"]
+],function(){
+Route::get("logout",[ApiController::class,"logout"]);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
