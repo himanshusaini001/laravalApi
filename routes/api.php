a714f87ed76ace6176f36ffca988eaede3f0ffe1
himanshu <himanshusaini26112002@gmail.com>
2024-05-20 13:21:10 +0530

    modified

--- routes/api.php
+++ routes/api.php
@@ -20,21 +20,23 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
 
     // Logout
     Route::get("logout", [ApiController::class,"logout"]);
+
+    // Product Data
+
+    // Post Product Data
+    Route::post("post_product",[ApiController::class,"post_product"]);
+
+    // Get All Product Data
+    Route::get('get_product_list', [ApiController::class, 'get_product_list']);
+    
+    // Get Single Product Data
+    Route::get('get_single_data/{product_id}', [ApiController::class, 'get_single_data']);
+
+      //Delete Product Data
+      Route::get('delete_product_data/{id}', [ApiController::class, 'delete_product_data']);
+
 });
 
-// Product Data
-
-// Post Product Data
- Route::post("post_product",[ApiController::class,"post_product"]);
-
- // Get All Product Data
- Route::get('get_product_list', [ApiController::class, 'get_product_list']);
- 
- // Get Single Product Data
- Route::get('get_single_data/{product_id}', [ApiController::class, 'get_single_data']);
-
-  //Delete Product Data
-  Route::get('delete_product_data/{id}', [ApiController::class, 'delete_product_data']);
 
 
 // Route::get('/user', function (Request $request) {
