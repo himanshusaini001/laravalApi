82b9d19a01ac46447cfa4e8f12351f1ffc1cdb69
himanshu <himanshusaini26112002@gmail.com>
2024-05-22 10:23:37 +0530

    modified

--- routes/api.php
+++ routes/api.php
@@ -5,28 +5,33 @@ use App\Http\Controllers\Api\ApiController;
 use Illuminate\Support\Facades\Route;
 use Whoops\Run;
 
+// User Route
+
 // Register
-Route::post("register",[ApiController::class,"register"]);
+Route::post("user/register",[ApiController::class,"register"]);
 
 // Login
-Route::post("login",[ApiController::class,"login"]);
+Route::post("user/login",[ApiController::class,"login"]);
 
-Route::group(["middleware" => ["auth:sanctum",'admin']], function () {
+Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum', 'admin']], function () {
 
-    // single Data Fetch
+    // User Profile 
     Route::get("profile", [ApiController::class,"profile"]);
 
     // Logout
     Route::get("logout", [ApiController::class,"logout"]);
-    // Product Data
+});
 
-    // Post Product Data
+// Product Route
+ Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum', 'admin']], function () {
+
+    // Add Product Data
     Route::post("add_product",[ApiController::class,"add_product"]);
 
     // Get All Product Data
     Route::get('product_list', [ApiController::class, 'product_list']);
 
-    // Post Product Data
+    // Update Product Data
     Route::put("update_product/{id}",[ApiController::class,"update_product"]);
     
     //Delete Product Data
